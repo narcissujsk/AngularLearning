4b2a0c8184e3762eeb7bd9ad4faef293f50a1543
narcissujsk <1791121094@qq.com>
2018-06-13 17:16:08 +0800

    js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Artical} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  jsk = 'my first app';
+  articles: Artical[];
+  constructor() {
+    this.articles = [
+      new Artical('Angular 2', 'http://angular.io', 3),
+      new Artical('Fullsack', 'http://fullstack.io', 2),
+      new Artical('Angular Homepage', 'http://angular.io', 3),
+    ];
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Artical(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HiComponent } from './hi/hi.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { ArticleComponent } from './article/article.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HiComponent,
+    UserComponent,
+    UsersComponent,
+    ArticleComponent,
   ],
   imports: [
     BrowserModule
